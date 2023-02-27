e373ce8432b8fc1fca59861906688c62f88e240d
39Hz <whbbdx@gmail.com>
2023-02-27 14:47:55 +0800

    update rename_results

--- rename_results.py
+++ rename_results.py
@@ -1,19 +1,30 @@
 import os
+import shutil
 
-# 指定文件夹路径
-folder_path = 'RUNS'
+# 设置原始文件夹路径
+source_folder = 'yolo'
 
-# 遍历文件夹下的所有子文件夹和文件
-for subdir, _, files in os.walk(folder_path):
-    for file in files:
-        # 如果文件名是results.txt
-        if file == 'results.txt':
-            # 获取子文件夹名，用于构造新的文件名
-            subdir_name = os.path.basename(subdir)
-            # 构造新的文件名
-            new_file_name = f'results_of_{subdir_name}.txt'
-            # 构造文件的完整路径
-            file_path = os.path.join(subdir, file)
-            new_file_path = os.path.join(subdir, new_file_name)
-            # 重命名文件
-            os.rename(file_path, new_file_path)
+# 设置目标文件夹路径
+target_folder = 'yolo/results'
+
+# 获取原始文件夹中的所有子文件夹
+subfolders = [f.path for f in os.scandir(source_folder) if f.is_dir()]
+
+for subfolder in subfolders:
+    if os.path.basename(subfolder) == 'results':
+        continue
+
+    # 获取子文件夹名称
+    subfolder_name = os.path.basename(subfolder)
+
+    # 获取子文件夹中的result.txt文件路径
+    result_path = os.path.join(subfolder, 'results.txt')
+
+    # 构造目标文件名
+    target_name = f'results-{subfolder_name}.txt'
+
+    # 构造目标文件路径
+    target_path = os.path.join(target_folder, target_name)
+
+    # 复制文件到目标路径
+    shutil.copyfile(result_path, target_path)
