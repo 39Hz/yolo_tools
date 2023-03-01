64a0d580be879af740708c231107c2d2d9f1a66b
39Hz <whbbdx@gmail.com>
2023-03-01 15:29:48 +0800

    update rename_results,add file of colors

--- rename_results.py
+++ rename_results.py
@@ -1,30 +1,27 @@
 import os
 import shutil
 
-# 设置原始文件夹路径
-source_folder = 'yolo'
+# set the source folder path
+source_folder = r'folder'
 
-# 设置目标文件夹路径
-target_folder = 'yolo/results'
+# set the new folder path
+target_folder = r'folder/results'
 
-# 获取原始文件夹中的所有子文件夹
-subfolders = [f.path for f in os.scandir(source_folder) if f.is_dir()]
-
-for subfolder in subfolders:
-    if os.path.basename(subfolder) == 'results':
-        continue
-
-    # 获取子文件夹名称
-    subfolder_name = os.path.basename(subfolder)
-
-    # 获取子文件夹中的result.txt文件路径
-    result_path = os.path.join(subfolder, 'results.txt')
-
-    # 构造目标文件名
-    target_name = f'results-{subfolder_name}.txt'
-
-    # 构造目标文件路径
-    target_path = os.path.join(target_folder, target_name)
-
-    # 复制文件到目标路径
-    shutil.copyfile(result_path, target_path)
+# travers subfolders and all the files
+for subdir, _, files in os.walk(source_folder):
+    for file in files:
+        # if file name is results.txt
+        if file == 'results.txt':
+            # get name of the subdir
+            subdir_name = os.path.basename(subdir)
+            # generate new file name
+            new_file_name = f'results_of_{subdir_name}.txt'
+            # generate the fulll path
+            file_path = os.path.join(subdir, file)
+            new_file_path = os.path.join(subdir, new_file_name)
+            # rename the file
+            os.rename(file_path, new_file_path)
+            # generate target apth
+            target_path = os.path.join(target_path, new_file_name)
+            # copy renamed_files to the target folder
+            shutil.copyfile(new_file_path, target_path)
